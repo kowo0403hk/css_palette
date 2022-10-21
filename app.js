fb859c3826ded0b8a70f2b11bb793b6e4dd3a205
Alex Cheung <alexc.sfu@gmail.com>
2022-10-20 21:20:56 -0700

    added mouse movement script

---
+++ app.js
@@ -0,0 +1,20 @@
+const gallery = document.getElementById("gallery");
+
+window.onmousemove = (e) => {
+  const mouseX = e.clientX,
+    mouseY = e.clientY;
+
+  // get the ratio for mouse position
+  const xDecimal = mouseX / window.innerWidth,
+    yDecimal = mouseY / window.innerHeight;
+
+  const maxX = gallery.offsetWidth - window.innerWidth,
+    maxY = gallery.offsetHeight - window.innerHeight;
+
+  // get the respective mouse position of the gallery element based on the decimals
+  const panX = maxX * xDecimal * -1,
+    panY = maxY * yDecimal * -1;
+
+  // update the CSS to transform the gallery's position by panX/Y numbers of pixels
+  gallery.style.transform = `translate(${panX}px, ${panY}px)`;
+};
